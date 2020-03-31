bdd3a84439fdcbec44c57db9276d40b8dd77ce2c
Bajpan Gosh <bajpangosh@users.noreply.github.com>
2020-03-31 20:45:17 +0530

    Update install-erpnext.sh
    Update

--- install-erpnext.sh
+++ install-erpnext.sh
@@ -2,11 +2,13 @@
 # GET ALL USER INPUT
 echo "Username (eg:- give me a name)?"
 read username
-echo "ErpNext Installing... it will take 15 Min"
 sleep 2;
+echo "ErpNext Installing... it will take > 15 Min"
+sleep 3;
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install python3-minimal build-essential python3-setuptools pwgen -y
 sudo wget https://raw.githubusercontent.com/frappe/bench/master/playbooks/install.py
+ip=$(curl -s -X GET https://checkip.amazonaws.com)
 password=`pwgen -s 14 1`
 export LC_ALL=C.UTF-8
 sudo -H python3 install.py --production --site localhost --user $username --mysql-root-password $password --admin-password $password --bench-name $username
@@ -14,11 +16,13 @@ echo
 echo
 tput setaf 2; echo "Here is your ErpNext Credentials"
 echo "--------------------------------"
-echo "Website:    http://your droplet ip"
+echo "Website:    http://$ip"
 echo
 tput setaf 4; echo "Admin Name:       Administrator"
 tput setaf 4; echo "Admin Password:   $password"
-tput setaf 4; echo "Login Name:       $username"
+echo "--------------------------------"
+tput setaf 4; echo "Ubuntu Login Name:$username"
+tput setaf 4; echo "Ubuntu Password  :$password"
 echo "--------------------------------"
 tput sgr0
 echo
